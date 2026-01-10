bd6da78782a8a3b00f432a8f5a6b93d07ccd45f3
AndreyQA <marynichandrii@gmail.com>
2026-01-10 16:34:14 +0200

    Refactor test scenario

---
+++ src/tests/placeOrder.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+import HomePage from '../pages/homePage';
+import ProductPage from '../pages/productPage';
+import CartPage from '../pages/cartPage';
+
+import products from '../test-data/products.json';
